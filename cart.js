e23bd152a6cf345ff6c7734d694a1b9f8f14a538
drodols12 <72259687+drodols12@users.noreply.github.com>
2021-03-01 18:40:09 +0800

    Add files via upload

--- cart.js
+++ cart.js
@@ -1,24 +1,6 @@
 function getCake(){
     const cakeImg = document.getElementById("cakeImg");
     const ckNM = document.getElementById("cakeType2");
-
-
-    window.addEventListener('load', ()=>{
-    ckNM.textContent = localStorage.getItem('cakeNm');
-    cakeImg.setAttribute("src", localStorage.getItem('cake1'));
-
-    })
-}
-getCake();
-function Menu(){
-    const menu = document.querySelector(".menu");
-    
-    menu.onclick = function(){
-        document.location.href = "cakepage.html"
-    }
-}
-Menu();
-function incrementValue(){
     const add = document.getElementById("add");
     const minus = document.getElementById("minus");
     const prc = localStorage.getItem('val')
@@ -27,59 +9,81 @@ function incrementValue(){
     const rsv = document.getElementById("rsv");
     const getOrder = document.getElementById("date");
 
-    window.onload = function(){
-        minus.disabled = true;
+
+    window.addEventListener('load', ()=>{
+    ckNM.textContent = localStorage.getItem('cakeNm');
+    cakeImg.setAttribute("src", localStorage.getItem('cake1'));
+
+    })
+
+    
+        window.onload = function(){
+            minus.disabled = true;
+        }
+    
+    
+        add.addEventListener("click", ()=>{
+            // var value;
+            var price = parseInt(document.getElementById("price").value = prc);
+            var value = parseInt(document.getElementById("num").value, 10);
+            var total;
+            var today = new Date();
+            var dd = String(today.getDate()).padStart(2, '0');
+            var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+            var yyyy = today.getFullYear();
+    
+            today = mm + '/' + dd + '/' + yyyy;
+            
+            
+    
+                minus.disabled = false;
+                value++;
+                total = (value)*price;
+                    
+               totalval = document.getElementById("num").value = value;
+               totalprc = document.getElementById("price").value = total + "$";
+    
+    
+               rsv.onclick = function(){
+                    window.open('mailto:chosen1sbakeshop@gmail.com?subject=cake%20order&body=Cake you ordered: ' + ckNM.textContent + '%0D%0AQuantity: ' +totalval + '%0D%0ATotal Price: ' + totalprc + '%0D%0ADate ordered: ' + today + "%0D%0ADelivery Date: " + getOrder.value);
+                   
+               }
+        })
+        
+        minus.addEventListener("click", ()=>{
+            var price = parseInt(document.getElementById("price").value = prc);
+            var value = parseInt(document.getElementById("num").value, 10);
+            
+            var total;
+    
+            
+    
+                total = (value*price)-price;
+                value = value - 1;
+    
+                
+                if((value <= 0 )||(value === 0 )){
+                    minus.disabled = true;
+                }
+                totalprc = document.getElementById("num").value = value;
+                totalval = document.getElementById("price").value = total + "$";
+            })
+        
+    }  
+
+getCake();
+function Menu(){
+    const menu = document.querySelector(".menu");
+    
+    menu.onclick = function(){
+        document.location.href = "cakepage.html"
     }
 
 
-    add.addEventListener("click", ()=>{
-        // var value;
-        var price = parseInt(document.getElementById("price").value = prc);
-        var value = parseInt(document.getElementById("num").value, 10);
-        var total;
-        var today = new Date();
-        var dd = String(today.getDate()).padStart(2, '0');
-        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-        var yyyy = today.getFullYear();
-
-        today = mm + '/' + dd + '/' + yyyy;
-        
-
-            minus.disabled = false;
-            value++;
-            total = (value)*price;
-                
-           totalval = document.getElementById("num").value = value;
-           totalprc = document.getElementById("price").value = total + "$";
-
-
-           rsv.onclick = function(){
-                window.open('mailto:chosen1sbakeshop@gmail.com?subject=cake%20order&body=value: ' +totalval + '%0D%0Atotal price: ' + totalprc + '%0D%0Adate ordered: ' + today + "%0D%0ADelivery Date: " + getOrder.value);
-               
-           }
-    })
-    
-    minus.addEventListener("click", ()=>{
-        var price = parseInt(document.getElementById("price").value = prc);
-        var value = parseInt(document.getElementById("num").value, 10);
-        
-        var total;
-
-        
-
-            total = (value*price)-price;
-            value = value - 1;
-
-            
-            if((value <= 0 )||(value === 0 )){
-                minus.disabled = true;
-            }
-            totalprc = document.getElementById("num").value = value;
-            totalval = document.getElementById("price").value = total + "$";
-        })
     
 }
-incrementValue();   
+Menu();
+
 function navSlides() {
     const burger = document.querySelector(".burger");
     const nav = document.querySelector(".nav-link");
